e48278aaeffec22867c0566a0fde6f35e075289b
joelc74 <joelc74@hotmail.com>
2025-10-04 17:31:02 +0200

    Atualizacion backend

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 const db = require("./models");
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(cors(corsOptions));
 
 //Sincronizar BD
 db.sequelize.sync().then(() => {
